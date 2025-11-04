9a97d8ad07c8b00900f4445bfc52bc9f8dbe3f86
Austin Berry <austin.berry@docker.com>
2025-11-04 10:25:08 -0500

    remove duplicate logic

--- pkg/terminal/terminal.go
+++ pkg/terminal/terminal.go
@@ -9,12 +9,7 @@ import (
 // GetWidth returns the width of the terminal in characters.
 // If the width cannot be determined, it returns a default value of 120.
 func GetWidth() int {
-	fd, _ := term.GetFdInfo(os.Stdout)
-	ws, err := term.GetWinsize(fd)
-	if err != nil {
-		return 120
-	}
-	return int(ws.Width)
+	return GetWidthFrom(os.Stdout)
 }
 
 // GetWidthFrom returns the width of the terminal from a given output stream.
